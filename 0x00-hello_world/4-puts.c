0413ee6445a1cf552fe31252b4ceedb51f6751c5
kingbyno <kingbyno007@gmail.com>
2022-06-30 19:09:48 -0700

    four puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /
- *main - Entry point
- *Return: Always 0 (sucess)
+ * main - Entry point
+ * Return: Always 0 (sucess)
  */
 int main(void)
 {
-puts("\"programming is like bulding a multilingual puzzle");
+puts("\"Programming is like bulding a multilingual puzzle");
 return (0);
 }
