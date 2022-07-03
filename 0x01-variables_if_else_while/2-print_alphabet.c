4260b7472feef7db9bb484bf7c50014cbc842606
kingbyno <kingbyno007@gmail.com>
2022-07-02 23:15:10 -0700

    print a

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 /**
   * main - Entry point
   *
@@ -5,9 +6,9 @@
   */
 int main(void)
 {
-	char letter;
-	for (letter = 'a'; letter <= 'z'; letter++)
-		putchar(letter);
-	putchar('\n');
-	return (0);
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+putchar(letter);
+putchar('\n');
+return (0);
 }
