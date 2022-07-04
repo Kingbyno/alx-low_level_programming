fb1267391d17ba4e3121b97fcfb65d727adda414
kingbyno <kingbyno007@gmail.com>
2022-07-04 01:35:50 -0700

    tired 4A print

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,10 +5,10 @@
  * Return: Always 0
  */
 int main(void)
-{char a; for (a = 'a'; a <= 'z'; a++)
-{if (a == 'q' || a == e)
+{
+char a; for (a = 'a'; a <= 'z'; a++)
+if (a == 'q' && a == e)
 continue;
-}
 putchar ('\n');
 return (0);
 }
