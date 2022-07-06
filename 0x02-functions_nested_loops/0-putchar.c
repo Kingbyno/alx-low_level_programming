ddc516dc71ed4c1cd420ea249a004a4b993c8a4a
kingbyno <kingbyno007@gmail.com>
2022-07-05 21:08:57 -0700

    hot change

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,25 @@
 #include "main.h"
-/**
- * main - prints putchar
- * Return: always 0
- */
-int main(void)
-{
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
 
-return (0);
+
+
+/**
+ *  Write a program that prints "_putchar", followed by a new line
+ *  Return: Always 0
+ */
+
+int main(void)
+
+{
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar("\n");
+
+	return (0);
+
 }
