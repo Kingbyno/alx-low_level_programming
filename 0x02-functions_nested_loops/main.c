7c05140ecc7263388adcfdbf6d19fd7863f93df8
kingbyno <kingbyno007@gmail.com>
2022-07-07 01:25:04 -0700

    check fix

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,23 +1,7 @@
 #include "main.h"
 
-/**
- * main - entery point
- *
- * Return: 0
- */
-int main(void)
-{
-	print _putchar(char c);
-	void print_alphabet(void);
+print _putchar(char c);
+void print_alphabet(void);
 
 
 
-
-
-
-
-
-	_putchar('\n');
-	return (0);
-
-}
