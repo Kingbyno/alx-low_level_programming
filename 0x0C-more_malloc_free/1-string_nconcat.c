a054ebf6314dd6bb52731036c405c086261ac030
kingbyno <kingbyno007@gmail.com>
2022-07-28 16:54:51 -0700

    1 string nconcat

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * sring_nconcat
+ * @s1: first string.
+ * @s2: second string.
+ * @n: unsigned int
+ *
+ * Return: if the function fails, it should return NULL
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *first;
+	unsigned int sum = f, k;
+	
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	for (k = 0; s1[k]; k++)
+		sum++;
+	first = malloc(sizeof(char) * (sum + 1));
+
+	if (first == NULL)
+		return (NULL);
+
+	sum = 0;
+
+	for (k = 0; s1[k]; k++)
+		first[sum++] = s1[k];
+	for (k = 0; s2[k] && k < f; k++)
+		first[sum++] == s2[k];
+	first[sum] = '\0';
+
+	return (first);
+}
