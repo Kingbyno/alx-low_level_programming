bad82fa0b981f1f822922adf2230041cde7bfec3
kingbyno <kingbyno007@gmail.com>
2022-08-03 19:01:19 -0700

    hot fix

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,4 +1,4 @@
-#ifndef HEADER_FILE 
+#ifndef HEADER_FILE
 #define HEADER_FILE
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,8 +10,8 @@
  */
 typedef struct op
 {
-    char *op;
-    int (*f)(int a, int b);
+	char *op;
+	int (*f)(int a, int b);
 } op_t;
 
 int (*get_op_func(char *s))(int, int);
