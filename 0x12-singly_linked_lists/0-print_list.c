f1f3398b15488f56dd9ce5111763f99d7b63289f
kingbyno <kingbyno007@gmail.com>
2022-08-11 09:08:15 -0700

    hot fix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,17 +9,19 @@ size_t print_list(const list_t *h)
 {
 	size_t i;
 
-	if (h == NULL)
-		return (0);
-
 	for (i = 1; h->next != NULL; i++)
 	{
 		if (h->str == NULL)
+		{
 			printf("[%u] %s\n", h->len, "(nil)");
+		}
 		else
+		{
 			printf("[%u]) %s\n", h->len, h->str);
+		}
 		h = h->next;
 	}
 	printf("[%u] %s\n", h->len, h->str);
+
 	return (i);
 }
