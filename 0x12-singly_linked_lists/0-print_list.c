be7599183bc7d3cb64714acc81ffeeef963f5d7a
kingbyno <kingbyno007@gmail.com>
2022-08-11 19:45:18 -0700

    last check

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,23 +1,26 @@
 #include "lists.h"
 
 /**
- * print_list - print list items
- * @h: the list to print
- *
- * Description: Print each list item, prefixed by it's length, formatted using
- * "[%d] %s\n". If a list item is NULL, print it as "(nil)" with zero length.
- *
- * Return: size of the list
+ * print_list - check the code for Holberton School students.
+ * @h: name of the list
+ * Return: the number of nodes.
  */
 size_t print_list(const list_t *h)
 {
-if (h)
-{
-if (h->str)
-printf("[%d] %s\n", h->len, h->str);
-else
-printf("[0] (nil)\n");
-return (print_list(h->next) + 1);
-}
-return (0);
+	int count = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+		printf("[%d] %s\n", h->len, h->str);
+		}
+		count++;
+		h = h->next;
+	}
+	return (count);
 }
