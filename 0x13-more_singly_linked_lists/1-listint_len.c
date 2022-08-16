da83044dd5f5724bafa3e6cddcedda810b536395
kingbyno <kingbyno007@gmail.com>
2022-08-15 23:42:00 -0700

    return element

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * print_listint - function tha returns node list
+ * @h: a pointer to the node list
+ * Return: the number of element
+ */
+size_t listint_len(const listint_t *h)
+{
+	unsigned in count;
+
+	if (h == NULL)
+		return (0);
+	for (count = 0; h != NULL; count++)
+		h = h->next;
+	return (count);
+}
